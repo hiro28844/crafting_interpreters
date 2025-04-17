d7e1d122a8af14cf1a5873c33b102f3292c5044f
Hiroyuki Aono <hiroyuki_aono@pkshatech.com>
2025-04-17 10:33:12 +0900

    implement chapter15 challenge1

--- clox/main.c
+++ clox/main.c
@@ -3,34 +3,136 @@
 #include "debug.h"
 #include "vm.h"
 
-int main(int argc, const char* argv[]) {
+void challenge1_1() {
     initVM();
 
     Chunk chunk;
     initChunk(&chunk);
 
-    int constant = addConstant(&chunk, 1.2);
+    int constant = addConstant(&chunk, 1);
     writeChunk(&chunk, OP_CONSTANT, 123);
     writeChunk(&chunk, constant, 123);
 
-    constant = addConstant(&chunk, 3.4);
+    constant = addConstant(&chunk, 2);
+    writeChunk(&chunk, OP_CONSTANT, 123);
+    writeChunk(&chunk, constant, 123);
+
+    writeChunk(&chunk, OP_MULTIPLY, 123);
+
+    constant = addConstant(&chunk, 3);
     writeChunk(&chunk, OP_CONSTANT, 123);
     writeChunk(&chunk, constant, 123);
 
     writeChunk(&chunk, OP_ADD, 123);
-
-    constant = addConstant(&chunk, 5.6);
-    writeChunk(&chunk, OP_CONSTANT, 123);
-    writeChunk(&chunk, constant, 123);
-
-    writeChunk(&chunk, OP_DIVIDE, 123);
-    writeChunk(&chunk, OP_NEGATE, 123);
-
     writeChunk(&chunk, OP_RETURN, 123);
 
-    disassembleChunk(&chunk, "test chunk");
+    disassembleChunk(&chunk, "1 * 2 + 3");
     interpret(&chunk);
     freeVM();
     freeChunk(&chunk);
+}
+
+void challenge1_2() {
+    initVM();
+
+    Chunk chunk;
+    initChunk(&chunk);
+
+    int constant = addConstant(&chunk, 1);
+    writeChunk(&chunk, OP_CONSTANT, 123);
+    writeChunk(&chunk, constant, 123);
+
+    constant = addConstant(&chunk, 2);
+    writeChunk(&chunk, OP_CONSTANT, 123);
+    writeChunk(&chunk, constant, 123);
+
+    constant = addConstant(&chunk, 3);
+    writeChunk(&chunk, OP_CONSTANT, 123);
+    writeChunk(&chunk, constant, 123);
+
+    writeChunk(&chunk, OP_MULTIPLY, 123);
+    writeChunk(&chunk, OP_ADD, 123);
+    writeChunk(&chunk, OP_RETURN, 123);
+
+    disassembleChunk(&chunk, "1 + 2 * 3");
+    interpret(&chunk);
+    freeVM();
+    freeChunk(&chunk);
+}
+
+void challenge1_3() {
+    initVM();
+
+    Chunk chunk;
+    initChunk(&chunk);
+
+    int constant = addConstant(&chunk, 3);
+    writeChunk(&chunk, OP_CONSTANT, 123);
+    writeChunk(&chunk, constant, 123);
+
+    constant = addConstant(&chunk, 2);
+    writeChunk(&chunk, OP_CONSTANT, 123);
+    writeChunk(&chunk, constant, 123);
+
+    writeChunk(&chunk, OP_SUBTRACT, 123);
+
+    constant = addConstant(&chunk, 1);
+    writeChunk(&chunk, OP_CONSTANT, 123);
+    writeChunk(&chunk, constant, 123);
+
+    writeChunk(&chunk, OP_SUBTRACT, 123);
+    writeChunk(&chunk, OP_RETURN, 123);
+
+    disassembleChunk(&chunk, "3 - 2 - 1");
+    interpret(&chunk);
+    freeVM();
+    freeChunk(&chunk);
+}
+
+void challenge1_4() {
+    initVM();
+
+    Chunk chunk;
+    initChunk(&chunk);
+
+    int constant = addConstant(&chunk, 1);
+    writeChunk(&chunk, OP_CONSTANT, 123);
+    writeChunk(&chunk, constant, 123);
+
+    constant = addConstant(&chunk, 2);
+    writeChunk(&chunk, OP_CONSTANT, 123);
+    writeChunk(&chunk, constant, 123);
+
+    constant = addConstant(&chunk, 3);
+    writeChunk(&chunk, OP_CONSTANT, 123);
+    writeChunk(&chunk, constant, 123);
+
+    writeChunk(&chunk, OP_MULTIPLY, 123);
+    writeChunk(&chunk, OP_ADD, 123);
+
+    constant = addConstant(&chunk, 4);
+    writeChunk(&chunk, OP_CONSTANT, 123);
+    writeChunk(&chunk, constant, 123);
+
+    constant = addConstant(&chunk, 5);
+    writeChunk(&chunk, OP_CONSTANT, 123);
+    writeChunk(&chunk, constant, 123);
+    writeChunk(&chunk, OP_NEGATE, 123);
+
+    writeChunk(&chunk, OP_DIVIDE, 123);
+    writeChunk(&chunk, OP_SUBTRACT, 123);
+    writeChunk(&chunk, OP_RETURN, 123);
+
+    disassembleChunk(&chunk, "1 + 2 * 3 - 4 / -5");
+    interpret(&chunk);
+    freeVM();
+    freeChunk(&chunk);
+}
+
+int main(int argc, const char* argv[]) {
+    challenge1_1();
+    challenge1_2();
+    challenge1_3();
+    challenge1_4();
     return 0;
 }
