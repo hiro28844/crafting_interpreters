cf2b24c421b0299db02c3f14965016349edcf0a2
Hiroyuki Aono <hiroyuki_aono@pkshatech.com>
2025-04-17 10:39:48 +0900

    implement chapter15 challenge2

--- clox/main.c
+++ clox/main.c
@@ -3,34 +3,73 @@
 #include "debug.h"
 #include "vm.h"
 
-int main(int argc, const char* argv[]) {
+void without_negate() {
     initVM();
 
     Chunk chunk;
     initChunk(&chunk);
 
-    int constant = addConstant(&chunk, 1.2);
+    int constant = addConstant(&chunk, 4);
     writeChunk(&chunk, OP_CONSTANT, 123);
     writeChunk(&chunk, constant, 123);
 
-    constant = addConstant(&chunk, 3.4);
+    constant = addConstant(&chunk, 3);
     writeChunk(&chunk, OP_CONSTANT, 123);
     writeChunk(&chunk, constant, 123);
 
-    writeChunk(&chunk, OP_ADD, 123);
-
-    constant = addConstant(&chunk, 5.6);
+    constant = addConstant(&chunk, 0);
     writeChunk(&chunk, OP_CONSTANT, 123);
     writeChunk(&chunk, constant, 123);
 
-    writeChunk(&chunk, OP_DIVIDE, 123);
-    writeChunk(&chunk, OP_NEGATE, 123);
+    constant = addConstant(&chunk, 2);
+    writeChunk(&chunk, OP_CONSTANT, 123);
+    writeChunk(&chunk, constant, 123);
+
+    writeChunk(&chunk, OP_SUBTRACT, 123);
+    writeChunk(&chunk, OP_MULTIPLY, 123);
+    writeChunk(&chunk, OP_SUBTRACT, 123);
 
     writeChunk(&chunk, OP_RETURN, 123);
 
-    disassembleChunk(&chunk, "test chunk");
+    disassembleChunk(&chunk, "without negate");
     interpret(&chunk);
     freeVM();
     freeChunk(&chunk);
+}
+
+void without_subtract() {
+    initVM();
+
+    Chunk chunk;
+    initChunk(&chunk);
+
+    int constant = addConstant(&chunk, 4);
+    writeChunk(&chunk, OP_CONSTANT, 123);
+    writeChunk(&chunk, constant, 123);
+
+    constant = addConstant(&chunk, 3);
+    writeChunk(&chunk, OP_CONSTANT, 123);
+    writeChunk(&chunk, constant, 123);
+    writeChunk(&chunk, OP_NEGATE, 123);
+
+    constant = addConstant(&chunk, 2);
+    writeChunk(&chunk, OP_CONSTANT, 123);
+    writeChunk(&chunk, constant, 123);
+    writeChunk(&chunk, OP_NEGATE, 123);
+
+    writeChunk(&chunk, OP_MULTIPLY, 123);
+    writeChunk(&chunk, OP_ADD, 123);
+
+    writeChunk(&chunk, OP_RETURN, 123);
+
+    disassembleChunk(&chunk, "without subtract");
+    interpret(&chunk);
+    freeVM();
+    freeChunk(&chunk);
+}
+
+int main(int argc, const char* argv[]) {
+    without_negate();
+    without_subtract();
     return 0;
 }
